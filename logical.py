6eefd79ae81ca2ccf9f9fd09da05854f95f5419f
Swaroop Talupula <swarooptalupulav@gmail.com>
2026-09-08 13:27:39 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,13 @@
+
+a = 5
+result= a > 2 and a < 10
+print("result of ",a, ">", 2,"and", a, "<", 10,"is: ", result)
+
+b = 5
+result2 = b < 3 or b > 10
+print("result of ",b, "<", 3,"or", b, ">", 10,"is: ", result2)
+
+
+c = 5
+result3 = not(c > 3)
+print("result of not(",c, ">", 3,") is: ", result3)
